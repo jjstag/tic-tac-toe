448883712ced2ed12773150ffeebe87dd97a7113
Stag <jstag7@gmail.com>
2025-10-28 10:53:29 -1000

    moved win handling outside of the win checking function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 const Gameboard = (function () {
     const board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     const place = (player, spot) => {
-    // player should be 1 or 2. spot should be 1-9
-        spot-- // make the spot begin at 0 
-        if (player == 2) {player = -1} // convert the board to a "1, 0, -1" format.
+        // player should be 1 or 2. spot should be 1-9
+        spot--
+        // make the spot begin at 0
+        if (player == 2) {player = -1}
+        // convert the board to a "1, 0, -1" format.
         function checkForWin(board) {
             const wins = [
                 [0, 1, 2],
@@ -22,14 +24,15 @@ const Gameboard = (function () {
                 }
                 console.log(rowValue)
                 if (Math.abs(rowValue) === 3) {
-                    console.log(`Player ${player} won!`)
                     return true
                 }
             }
             
         }
         board[spot] = player
-        checkForWin(board)
+        if (checkForWin(board) === true) {
+            console.log(`Player ${player} won!`)
+        }
         return board
     } 
     return {
